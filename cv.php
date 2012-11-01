8d8926c7f5ebb78c0473b64f3d0d1291fdfaabd7
Nicholas Avenell <nicholas@aquarionics.com>
2012-11-01 22:36:15 +0000

    Initial Commit

---
+++ cv.php
@@ -0,0 +1,11 @@
+<?PHP
+	//include("CV.html");
+	readfile("https://docs.google.com/View?id=df5kjdcb_33648g4tcg");
+?>
+
+<div id="footer">
+&copy; Nicholas Avenell 2007. That's just the content, feel free to steal the code.
+</div>
+
+</body>
+</html>
